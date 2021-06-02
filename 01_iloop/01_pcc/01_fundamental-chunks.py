0b68d428a972e94c92841673c737c20f1918da97
Nikhil Thomas <nikhilthomas1@gmail.com>
2021-06-02 19:52:28 +0530

    Add simple python concepts

---
+++ 01_iloop/01_pcc/01_fundamental-chunks.py
@@ -0,0 +1,10 @@
+message = "Hello Python World!"
+print(message)
+name = "jackson silversmith"
+print(name.title(), name.upper(), name.lower())
+fName = "james"
+lName = "gamer"
+print(f"{fName} {lName}")
+print("\tbruce")
+print(":", " jon ".rstrip().lstrip(), ":")
+print(":", " jon ", ":")
